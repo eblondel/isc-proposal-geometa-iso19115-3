4219c5471af32320b3b68e84341bac27a7d5bea0
eblondel <emmanuel.blondel1@gmail.com>
2023-03-08 00:20:03 +0100

    proposal

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,6 @@
-proposal<-"Boilerplate ISC Proposal"
+proposal<-"ISO 19115-3 standard implementation in geometa R package"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
+author<-"Emmanuel Blondel"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
